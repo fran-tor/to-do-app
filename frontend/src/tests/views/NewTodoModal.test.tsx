9894257856455a45bfba5c4d5b7eeb01b3103071
fran-tor <jose.guido@encora.com>
2025-01-30 18:03:18 -0600

    test: add new todo modal tests

---
+++ frontend/src/tests/views/NewTodoModal.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react';
+import { it, expect, describe, vi, afterEach } from 'vitest'
+import '@testing-library/jest-dom/vitest';
+import { Todo } from '../../types';
+import NewTodoModal from '../../views/NewTodoModal';
+
+describe('NewTodoModal', () => {
+  const mockHandleClose = vi.fn();
+  const mockOnTodoEdited = vi.fn();
+
+  afterEach(() => {
+    vi.clearAllMocks();
+    document.body.innerHTML = '';
+  });
+
+  it('renders correctly when open', () => {
+    render(
+      <NewTodoModal isOpen={true} handleClose={mockHandleClose} onTodoAdded={mockOnTodoEdited} />
+    );
+
+    expect(screen.getByText('Create a new To Do')).toBeInTheDocument();
+  });
+
+  it('does not render when closed', () => {
+    render(<NewTodoModal isOpen={false} handleClose={mockHandleClose} onTodoAdded={mockOnTodoEdited} />)
+    screen.debug();
+    expect(screen.queryByText('Create a new To Do')).not.toBeInTheDocument();
+  });
+});
